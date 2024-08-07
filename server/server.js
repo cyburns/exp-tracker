27f41abb0affe0174bc971828b0e76517ade5d76
cyburns <72774499+cyburns@users.noreply.github.com>
2024-08-07 19:54:06 -0400

    Ga

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ import cors from "cors";
 import http from "http";
 import express from "express";
 import dotenv from "dotenv";
+import passport from "passport";
+import session from "express-session";
+import ConnectMongo from "connect-mongodb-session";
 
 dotenv.config();
 
@@ -17,12 +20,28 @@ const httpServer = http.createServer(app);
 
 // Connect to the database before starting the server
 await connectToDB();
+const MongoDBStore = ConnectMongo(session);
+
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
+});
+
+store.on("error", (error) => console.error(error));
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+    httpOnly: true,
+  },
+  store,
 });
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 await server.start();
 
@@ -35,6 +54,29 @@ app.use(
     context: async ({ req }) => ({ token: req.headers.token }),
   })
 );
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store,
+  })
+);
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: "Express error handler caught unknown middleware",
+    status: 500,
+    message: { err: "An error occurred" },
+  };
+
+  const errorObj = Object.assign({}, defaultErr, err);
+  return res.status(errorObj.status).json(errorObj);
+});
+
+app.use("/*", (req, res) => res.status(404).send("Page not found"));
 
 // Start the HTTP server after the Apollo Server is set up
 await new Promise((resolve) =>
