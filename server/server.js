4a1ec7be26be9090f25a050434b0790d69017da0
cyburns <72774499+cyburns@users.noreply.github.com>
2024-08-07 11:46:31 -0400

    switched from stand alone server to express

--- server/server.js
+++ server/server.js
@@ -1,14 +1,40 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./typeDefs/index.js";
 import { resolvers } from "./resolvers/index.js";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import cors from "cors";
+import http from "http";
+import express from "express";
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const SERVER_PORT = 3001;
 
-const { url } = await startStandaloneServer(server, {
-  listen: {
-    port: 3001,
-  },
+const app = express();
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-console.log(`Server is running on ${url}`);
+await server.start();
+
+// instance before passing the instance to `expressMiddleware`
+app.use(
+  "/",
+  cors(),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+);
+
+// Modified server startup
+await new Promise((resolve) =>
+  httpServer.listen({ port: SERVER_PORT }, resolve)
+);
+
+console.log(`🚀 Server ready at http://localhost:${SERVER_PORT}/`);
