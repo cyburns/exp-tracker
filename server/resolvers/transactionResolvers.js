755b49dbe37941d6f8b50f86291f0c8ee5d58c29
cyburns <72774499+cyburns@users.noreply.github.com>
2024-08-07 20:34:42 -0400

    transaction resolvers

--- server/resolvers/transactionResolvers.js
+++ server/resolvers/transactionResolvers.js
@@ -1,3 +1,112 @@
+import Transaction from "../models/transaction.model.js";
+import User from "../db/models/user.js";
+import Transaction from "../db/models/transaction.js";
 
+const transactionResolver = {
+  Query: {
+    transactions: async (_, __, context) => {
+      try {
+        if (!context.getUser()) throw new Error("Unauthorized");
+        const userId = await context.getUser()._id;
 
-export const transactionResolvers = {};
+        const transactions = await Transaction.find({ userId });
+        return transactions;
+      } catch (err) {
+        console.error("Error getting transactions:", err);
+        throw new Error("Error getting transactions");
+      }
+    },
+
+    transaction: async (_, { transactionId }) => {
+      try {
+        const transaction = await Transaction.findById(transactionId);
+        return transaction;
+      } catch (err) {
+        console.error("Error getting transaction:", err);
+        throw new Error("Error getting transaction");
+      }
+    },
+
+    categoryStatistics: async (_, __, context) => {
+      if (!context.getUser()) throw new Error("Unauthorized");
+
+      const userId = context.getUser()._id;
+      const transactions = await Transaction.find({ userId });
+      const categoryMap = {};
+
+      transactions.forEach((transaction) => {
+        if (!categoryMap[transaction.category]) {
+          categoryMap[transaction.category] = 0;
+        }
+
+        categoryMap[transaction.category] += transaction.amount;
+      });
+
+      return Object.entries(categoryMap).map(([category, totalAmount]) => ({
+        category,
+        totalAmount,
+      }));
+    },
+  },
+
+  Mutation: {
+    createTransaction: async (_, { input }, context) => {
+      try {
+        const newTransaction = new Transaction({
+          ...input,
+          userId: context.getUser()._id,
+        });
+
+        await newTransaction.save();
+        return newTransaction;
+      } catch (error) {
+        console.error("Error creating transaction:", error);
+        throw new Error("Error creating transaction");
+      }
+    },
+
+    updateTransaction: async (_, { input }) => {
+      try {
+        const updatedTransaction = await Transaction.findByIdAndUpdate(
+          input.transactionId,
+          input,
+          {
+            new: true,
+          }
+        );
+
+        return updatedTransaction;
+      } catch (error) {
+        console.error("Error updating transaction:", error);
+        throw new Error("Error updating transaction");
+      }
+    },
+
+    deleteTransaction: async (_, { transactionId }) => {
+      try {
+        const deletedTransaction = await Transaction.findByIdAndDelete(
+          transactionId
+        );
+        return deletedTransaction;
+      } catch (error) {
+        console.error("Error deleting transaction:", error);
+        throw new Error("Error deleting transaction");
+      }
+    },
+  },
+
+  Transaction: {
+    user: async (parent) => {
+      const userId = parent.userId;
+      try {
+        const user = await User.findById(userId);
+        return user;
+      } catch (error) {
+        console.error("Error getting user:", error);
+        throw new Error("Error getting user");
+      }
+    },
+  },
+};
+
+export default transactionResolver;
