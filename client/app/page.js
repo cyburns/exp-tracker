8924fdca8de718cf1aabed3e248da8dc67550c64
cyburns <72774499+cyburns@users.noreply.github.com>
2024-08-09 08:58:31 -0400

    styling

--- client/app/page.js
+++ client/app/page.js
@@ -4,6 +4,7 @@ import HomePage from "@/components/HomePage";
 import LoginPage from "@/components/LogInPage";
 import { useQuery } from "@apollo/client";
 import { GET_AUTH_USER } from "@/graphql/queries/user-queries";
+import Header from "@/components/Header";
 
 export default function Home() {
   const { loading, error, data } = useQuery(GET_AUTH_USER);
@@ -24,6 +25,7 @@ export default function Home() {
 
   return (
     <main>
+      <Header />
       <div>{data.authUser ? <HomePage /> : <LoginPage />}</div>
     </main>
   );
--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -51,21 +51,18 @@ const HomePage = () => {
   return (
     <>
       <div className="flex flex-col gap-6 items-center max-w-7xl mx-auto z-20 relative justify-center">
-        <div className="flex items-center">
-          <p className="md:text-4xl text-2xl lg:text-4xl font-bold text-center relative z-50 mb-4 mr-4 bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-400 inline-block text-transparent bg-clip-text">
-            Spend wisely, track wisely
-          </p>
+        <div className="flex justify-between w-full max-w-4xl mb-10 items-center">
           <img
             src={"https://tecdn.b-cdn.net/img/new/avatars/2.webp"}
-            className="w-11 h-11 rounded-full border cursor-pointer"
+            className="w-12 h-12 rounded-full border cursor-pointer"
             alt="Avatar"
           />
-          {!loading && (
-            <MdLogout
-              className="mx-2 w-5 h-5 cursor-pointer"
-              onClick={handleLogout}
-            />
-          )}
+
+          <div className="group flex flex-row" onClick={handleLogout}>
+            <p className="group-hover:underline">Log out</p>
+            {!loading && <MdLogout className="mx-2 w-5 h-5 cursor-pointer" />}
+          </div>
+
           {/* loading spinner */}
           {loading && (
             <div className="w-6 h-6 border-t-2 border-b-2 mx-2 rounded-full animate-spin" />
